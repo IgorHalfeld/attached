d3430939c1f51b3dd44f5d96e616bafffc46038d
Igor Luíz <igor@devstacks.com.br>
2017-06-02 05:17:13 -0300

    Start prototyping

---
+++ README.md
@@ -0,0 +1,1 @@
+### Just a prototyping so far..
---
+++ attached.js
@@ -0,0 +1,129 @@
+
+(function ($, window, undefined) {
+
+  var Observer = function() {
+    this.subs = {};
+  };
+
+  /**
+   * Notify a key calling your handler
+   * @param  {String} key
+   */
+  Observer.prototype.notify = function(key) {
+    if (!this.subs[key]) return;
+    this.subs[key].forEach(function(handler) {
+      handler();
+    });
+  };
+
+  /**
+   * Register a callback to a key
+   * @param  {String} key
+   * @param  {Function} handler
+   */
+  Observer.prototype.register = function(key, handler) {
+    if (!this.subs[key]) this.subs[key] = []
+    this.subs[key].push(handler);
+  };
+
+  function Attached(ob) {
+    this.$subject = new Observer(ob);
+    this.initEngine(ob);
+    this.parserViewToModel(ob);
+    this.ob = ob;
+  }
+
+  /**
+   * Check each property of object
+   * @param  {Object} ob
+   */
+  Attached.prototype.initEngine = function(ob) {
+    for (var key in ob) {
+      if (ob.hasOwnProperty(key)) {
+        this.makeReactive(ob, key);
+      }
+    }
+  };
+
+  /**
+   * Make the object reactive to changes
+   * @param  {Object} ob
+   * @param  {Object} key
+   */
+  Attached.prototype.makeReactive = function(ob, key) {
+    var _value = ob[key];
+    var _self = this;
+
+    Object.defineProperty(ob, key, {
+      get: function() {
+        return _value;
+      },
+      set: function(newValue) {
+        _value = newValue;
+
+        _self.$subject.notify(key);
+      }
+    });
+    this.parserModelToView(ob);
+  };
+
+  /**
+   * Parser all data from model to view
+   * @param  {Object} ob
+   */
+  Attached.prototype.parserModelToView = function(ob) {
+    var _nodes = document.querySelectorAll('[att-bind]');
+    var _self = this;
+
+    _nodes.forEach(function(node) {
+      var key = node.attributes['att-bind'].value;
+
+      _self.checkObjectDepth(key, ob[key]);
+      node.textContent = ob[key];
+
+      _self.$subject.register(key, function() {
+        node.textContent = ob[key];
+      });
+    });
+  };
+
+  /**
+   * Parser all data from view to model
+   * @param  {Object} ob
+   */
+  Attached.prototype.parserViewToModel = function(ob) {
+    var _nodes = document.querySelectorAll('[att-model]');
+    var _self = this;
+
+    _nodes.forEach(function(node) {
+      var _key = node.attributes['att-model'].value;
+
+      node.addEventListener('input', function(evt) {
+        var _value = evt.target.value;
+        _self.ob[_key] = _value;
+      });
+    });
+  };
+
+  Attached.prototype.checkObjectDepth = function(property, value) {
+    console.log('Value without data treated', property);
+    var _keys = property.split(/\./g);
+    // var _ob = {};
+    // _ob[_keys[0]] = {};
+    // _ob[_keys[0]][_keys[1]] = value;
+
+    console.log('Final value', _keys);
+    // return _ob;
+  };
+
+  /**
+   * Init all applicatiom
+   * @param  {Object} options
+   */
+  $.fn.attached = function(options) {
+    console.log('Attached is started!');
+    console.log(options);
+
+    window.attached = new Attached(options);
+  };
+})(jQuery, window)
